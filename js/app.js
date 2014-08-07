4ea377aea2a6f69733b13813618b9f93a83ec9cc
Isaac Gasi <igasi@evolet>
2014-08-07 04:10:02 -0500

    added form

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 
-// Call to colorbox for iframe
 $(document).ready(function(){
 
-    $( ".pre" ).colorbox({iframe:true, width:"80%", height:"80%"});
   
+  // Call to colorbox for iframe pre-registro
+  $( ".pre" ).colorbox({
+    iframe:true,
+    innerWidth: "75%",
+    innerHeight: "75%", 
+  });
+
+  // Call to colorbox for iframe contacto
+  $( ".contacto" ).colorbox({
+    iframe:true,
+    innerWidth: "40%",
+    innerHeight: "40%", 
+  });    
+
+  //RWD colorbox
+  var x = $('mydiv').width();
+  var y = $('mydiv').height();
+  parent.$.colorbox.resize({width:x, height:y});
+
 });
